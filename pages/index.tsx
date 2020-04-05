4dc8540bae3fc89004d7c11090b9535b4e2c110d
Justin Molinyawe <joosen@local>
2020-04-04 21:53:34 -0700

    stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ export default function Index() {
     return (
       <div>
         <p>Hello Next.js</p>
-        <p>https://nextjs.org/learn/basics/navigate-between-pages</p>
+        <p>https://nextjs.org/learn/basics/create-dynamic-pages</p>
       </div>
     );
   }
